4592c1426ab6733767253b5db4c7d7c7eb5275ba
Holger Graef <holger.graef@gmail.com>
2015-11-26 11:24:56 +0100

    Save database in subfolder

--- SampleManagerWeb.py
+++ SampleManagerWeb.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database/data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 

d7007fb88b5bf24aef22835e58738687443e0425
Holger Graef <holger.graef@gmail.com>
2015-12-31 15:31:01 +0100

    added possibility to choose from SMB resources

--- app/browser/views.py
+++ app/browser/views.py
@@ -17,11 +17,21 @@ class FileTile:
 @browser.route('/', defaults={'address': ''})
 @browser.route('/<path:address>')
 def imagebrowser(address):
-    # for the moment we'll only use the first database entry
-    # TODO: add possibility to choose from SMBResources
+    # TODO: make sure resource names do not contain / or are .. or stuff like that
     address = address.rstrip('/')
-    print "address: ", address
-    resource = SMBResource.query.filter_by(id=1).first()
+    print address
+    resourcetable = SMBResource.query.all()
+    resources=[]
+    if(address == ""):  # in this case we need to present choice of servers
+        for i in resourcetable:
+            f = FileTile()
+            f.name = i.name
+            f.image = "/static/resource.png"
+            resources.append(f)
+        return render_template('browserframe.html', files=[], folders=[], resources=resources)
+
+    resource = SMBResource.query.filter_by(name=address.split("/")[0]).first()
+    address_on_server = "" if address.find("/") == -1 else address[address.find("/")+1:]
     client_machine_name = "SampleManagerWeb"
     server_ip = socket.gethostbyname(resource.serveraddr)
     image_extensions = [".jpg", ".jpeg", ".png"]
@@ -32,11 +42,11 @@ def imagebrowser(address):
 
     files = []
     folders = []
-    for i in conn.listPath(resource.sharename, address):
+    for i in conn.listPath(resource.sharename, address_on_server):
         if i.filename == '.':
             continue
-        if i.filename == ".." and address == '':
-            continue
+        #if i.filename == ".." and address == '': should not be problem anymore as we do resource navigation separately
+        #    continue
         f = FileTile()
         f.name, f.ext = os.path.splitext(i.filename)
         if not i.isDirectory:
@@ -52,14 +62,13 @@ def imagebrowser(address):
 
     files = sorted(files, key=lambda f: f.name)
     folders = sorted(folders, key=lambda f: f.name)
-    return render_template('browserframe.html', files=files, folders=folders, address=address)
+    return render_template('browserframe.html', files=files, folders=folders, resources=[] address=address)
 
 
 @browser.route('/img/<path:image>')
 def browserimage(image):
-    # for the moment we'll only use the first database entry
-    # TODO: add possibility to choose from SMBResources
-    resource = SMBResource.query.filter_by(id=1).first()
+    resource = SMBResource.query.filter_by(name=image.split("/")[0]).first()
+    address_on_server = image[image.find("/")+1:]
     client_machine_name = "SampleManagerWeb"
     server_ip = socket.gethostbyname(resource.serveraddr)
     image_extensions = [".jpg", ".jpeg", ".png"]
@@ -69,7 +78,7 @@ def browserimage(image):
     assert conn.connect(server_ip, 139)
 
     file_obj = tempfile.NamedTemporaryFile()
-    file_attributes, filesize = conn.retrieveFile(resource.sharename, image, file_obj)
+    file_attributes, filesize = conn.retrieveFile(resource.sharename, address_on_server, file_obj)
 
     file_obj.seek(0)
     image_binary = file_obj.read()

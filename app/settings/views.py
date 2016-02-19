59577402c1a38409533c82fc828099a36db96234
Holger Graef <holger.graef@lpa.ens.fr>
2016-02-19 11:28:23 +0100

    corrected bug where new users are not added on production server

--- app/settings/views.py
+++ app/settings/views.py
@@ -92,5 +92,6 @@ def users():
     if form.validate_on_submit():
         user = User(is_admin=form.is_admin.data, email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
+        db.session.commit()
         return redirect(url_for('settings.users'))
     return render_template('settings/users.html', users=User.query.all(), form=form)

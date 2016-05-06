81dbaac5a42192e986b5014c76a751c69f783b2c
Holger Graef <holger.graef@gmail.com>
2016-05-06 16:46:14 +0200

    Added function to find unused uploads and moved upload helper functions from manage.py to separate settings page

--- app/settings/views.py
+++ app/settings/views.py
@@ -1,16 +1,14 @@
 from flask import render_template, redirect, request, url_for
 from .. import db
 from ..decorators import admin_required
-from ..models import SampleType
-from ..models import ActionType
-from ..models import SMBResource
-from ..models import User
+from ..models import SampleType, ActionType, SMBResource, User, Upload, Action, Sample
 from forms import NewSMBResourceForm, NewTypeForm, ShutdownForm, NewUserForm
 from . import settings
 from flask.ext.login import login_required
 import git
 from config import basedir
 from flask import current_app as app
+import os
 
 # see http://flask.pocoo.org/snippets/67/
 def shutdown_server():
@@ -132,3 +130,82 @@ def revision():
         app.logger.error("Could not retrieve remote git information:" + str(type(inst)) + str(inst.args))
 
     return render_template('settings/revision.html', local_rev=local_revision, remote_rev=remote_revision, recent_changes=recent_changes)
+
+
+### two helper functions for the settings/uploads page
+def handle_img(loc, src, refdlist):
+    if src[:15] == '/browser/ulimg/':
+        #print loc, "uploaded image ID", src[15:]
+        refdlist.append(int(src[15:]))
+    elif src[:5] == 'data:':
+        #print loc, "base64 image"
+        pass
+    else:
+        #print loc, "other source:", src
+        pass
+
+def handle_img_tags(text, itemid, refdlist):
+    i = -1
+    while True:
+        i = text.find('<img', i+1)
+        if i != -1:
+            j = text.find('>', i)   # end of img tag
+            k = text.find('src=', i)
+            assert k<j                            # make sure the src attr. belongs to the img tag
+            l = k+4
+            invcomma = text[l]
+            m = text.find(invcomma, l+1)
+            assert m<j                            # make sure the string ends before the end of the img tag
+            handle_img("{} position {}".format(itemid, i), text[l+1:m], refdlist)
+        else:
+            break
+
+@settings.route('/uploads', methods=['GET'])
+@login_required
+@admin_required
+def uploads():
+    ##### Find duplicates and empty files
+    # could probably do this much better with an elegant DB query
+    uploads = Upload.query.all()
+    emptyfiles = []
+    duplicates = []
+    for i1 in range(len(uploads)):
+        u1 = uploads[i1]
+        # check file size
+        stat = os.stat(os.path.join(app.config['UPLOAD_FOLDER'], str(u1.id) + '.' + u1.ext))
+        if stat.st_size == 0:
+            #print "Upload ID {} is an empty file.".format(u1.id)
+            emptyfiles.append(u1)
+            continue
+        for i2 in range(i1, len(uploads)):  # only iterate through rest of uploads (avoid double counting)
+            u2 = uploads[i2]
+            if u1.id == u2.id: continue
+            if u1.hash == u2.hash:
+                #print "Upload ID {} is a duplicate of {}.".format(u1.id, u2.id)
+                duplicates.append((u1, u2))
+
+    ##### Scan all referenced images and find unused uploads
+    refdlist = []  # list of referenced images
+    unused = []     # list of unused uploads
+
+    # check for image tags in action descriptions
+    actions = Action.query.all()
+    for action in actions:
+        handle_img_tags(action.description, "action {}".format(action.id), refdlist)
+
+    # check for sample images and image tags in sample descriptions
+    samples = Sample.query.all()
+    for sample in samples:
+        if sample.image is not None:
+            handle_img("sample {}".format(sample.id), sample.image, refdlist)
+        if sample.description is not None:
+            handle_img_tags(sample.description, "sample {}".format(sample.id), refdlist)
+
+    # find unused uploads
+    uploads = Upload.query.all()
+    for u in uploads:
+        if u.id not in refdlist:
+            #print "Unused upload: ", u.id
+            unused.append(u)
+
+    return render_template('settings/uploads.html', emptyfiles=emptyfiles, duplicates=duplicates, unused=unused)

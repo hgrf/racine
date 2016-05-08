6d387940bb5234470f6966af76d14a6814f3df09
Holger Graef <holger.graef@gmail.com>
2016-05-08 20:03:22 +0200

    Added "glyphicon" indicator to indicate if navbar entry has children or not

--- app/static/editor.js
+++ app/static/editor.js
@@ -267,6 +267,45 @@
             event.preventDefault();
         });
 
+        // this function adds a glyphicon to the nav-entry $target
+        function addGlyphicon($target) {
+            if($($target.data('target')).is(":hidden"))
+                $target.prepend('<span class="glyphicon glyphicon-expand"></span>');
+            else
+                $target.prepend('<span class="glyphicon glyphicon-collapse-down"></span>');
+
+            // change the glyphicons when items are expanded or collapsed
+            $($target.data('target')).on("show.bs.collapse", function(e){
+               // the event goes up like a bubble and so it can affect parent nav-entries, if we do not pay
+               // attention where the event comes from
+               if($(this).attr('id') != e.target.id)
+                   return;
+               // $(this) now corresponds to the children container, we need to access the corresponding
+               // nav-entry using $(this).prev()
+               $(this).prev().children('.glyphicon').remove();
+               $(this).prev().prepend('<span class="glyphicon glyphicon-collapse-down"></span>');
+            });
+            $($target.data('target')).on("hide.bs.collapse", function(e){
+               if($(this).attr('id') != e.target.id)
+                   return;
+               $(this).prev().children('.glyphicon').remove();
+               $(this).prev().prepend('<span class="glyphicon glyphicon-expand"></span>');
+            });
+        }
+
+        function removeGlyphicon($target) {
+            $target.children('.glyphicon').remove();
+            $($target.data('target')).off('show.bs.collapse');
+            $($target.data('target')).off('hide.bs.collapse');
+        }
+
+        // add glyphicons to expandable items in the navbar
+        $('.nav-entry').each(function() {
+           if($($(this).data('target')).children().length > 0) {
+               addGlyphicon($(this));
+           }
+        });
+
         // initialise editor if sample is loaded
         if($("#sampleid").text() != "")
         {
@@ -306,6 +345,15 @@
                         if(data == "") {
                             var draggedItem = $( "#"+draggedId+".nav-container" );
                             if(parentId != 0) {
+                                if($("#children"+parentId).children().length == 0) {
+                                    // no children yet, so need to put glyphicon
+                                    addGlyphicon($("#children"+parentId).prev());
+                                }
+                                if(draggedItem.parent().children().length == 1)
+                                {
+                                    // there will be no children left after drag/drop, so remove glyphicon
+                                    removeGlyphicon(draggedItem.parent().prev());
+                                }
                                 draggedItem.appendTo("#children"+parentId);
                             } else {
                                 draggedItem.appendTo("#root");

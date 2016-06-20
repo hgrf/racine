ac447d7af4fe24db135ba637a09cac85f84698b5
Holger Graef <holger.graef@lpa.ens.fr>
2016-06-20 16:31:01 +0200

    added helper function to manage.py that repairs links that were created on previous server

--- manage.py
+++ manage.py
@@ -27,6 +27,54 @@ def uploadsizes():
         u.size = os.stat(os.path.join(app.config['UPLOAD_FOLDER'], str(u.id) + '.' + u.ext)).st_size
         db.session.commit()
 
+###################  TEMPORARY MODIFICATION THAT WILL CARRY OUT DB MODIFICATIONS
+from app.models import Action, Sample, SMBResource, Upload
+
+def handle_a_tags(text, itemid):
+    i = -1
+    while True:
+        i = text.find('<a', i+1)
+        if i != -1:
+            j = text.find('>', i)   # end of a tag
+            k = text.find('href=', i)
+            assert k<j                            # make sure the href attr. belongs to the a tag
+            l = k+5
+            invcomma = text[l]
+            m = text.find(invcomma, l+1)
+            assert m<j                            # make sure the string ends before the end of the a tag
+            print "{} position {}: {}".format(itemid, i, text[l+1:m])
+
+            # remove leading http(s)://server/ for affected links
+            if text[l+1:].startswith('http'):
+                if text[l+5] == 's':        # https
+                    n = text.find('/', l+9)
+                else:
+                    n = text.find('/', l+8)
+                text = text[:l+1]+text[m:n]+text[n:]
+        else:
+            break
+
+    return text
+
+@manager.command
+def linkhelper():
+    '''
+    This helper function automatically searches hyperlinks in sample and action descriptions.
+    '''
+
+    actions = Action.query.all()
+    for action in actions:
+        action.description = handle_a_tags(action.description, "action {}".format(action.id))
+        db.session.commit()
+
+    samples = Sample.query.all()
+    for sample in samples:
+        if sample.description is None: continue
+        sample.description = handle_a_tags(sample.description, "sample {}".format(sample.id))
+        db.session.commit()
+
+###################  END OF TEMPORARY MODIFICATION
+
 
 if __name__ == '__main__':
     manager.run()

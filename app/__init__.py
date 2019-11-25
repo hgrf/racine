65c09d3f1f8804de8071ef2cb91c4816704510ea
Holger Graef <holger.graef@gmail.com>
2019-11-25 01:26:01 +0100

    bugfix: handle exception when upgrading database

--- app/__init__.py
+++ app/__init__.py
@@ -43,22 +43,29 @@ def create_app(config_name):
     app.register_blueprint(printdata_blueprint, url_prefix='/print')
 
     # update activity types table
-    activity_types = ['selectsmbfile', 'login', 'logout']
     with app.app_context():
         from main.views import supported_targets
-        for key, target in supported_targets.items():
-            activity_types.append('add:'+key)
-            activity_types.append('delete:'+key)
-            for field in target['fields']:
-                activity_types.append('update:'+key+':'+field)
-
         from models import ActivityType
-        registered_activity_types = [at.description for at in ActivityType.query.all()]
-        for at in activity_types:
-            if at not in registered_activity_types:
-                newat = ActivityType(description=at)
-                db.session.add(newat)
-                db.session.commit()
+        from sqlalchemy.exc import OperationalError
+
+        activity_types = ['selectsmbfile', 'login', 'logout']
+        try:
+            registered_activity_types = [at.description for at in ActivityType.query.all()]
+
+            for key, target in supported_targets.items():
+                activity_types.append('add:' + key)
+                activity_types.append('delete:' + key)
+                for field in target['fields']:
+                    activity_types.append('update:' + key + ':' + field)
+
+            for at in activity_types:
+                if at not in registered_activity_types:
+                    newat = ActivityType(description=at)
+                    db.session.add(newat)
+                    db.session.commit()
+        except OperationalError as e:
+            # in case the table is not created yet, do nothing (this happens when we do 'python manage.py db upgrade')
+            pass
 
     # run usage statistics thread
     UsageStatisticsThread(app)

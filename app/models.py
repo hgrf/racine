84c76e58edc5268c1c04d806cc25eca162527360
Holger Graef <holger.graef@gmail.com>
2019-12-16 17:16:55 +0100

    updated sample and share deletion handlers to work with isdeleted flag

--- app/models.py
+++ app/models.py
@@ -10,38 +10,69 @@ from datetime import datetime
 
 SAMPLE_NAME_LENGTH = 64
 
+
+def deleted_sample_handler(session, sample):
+    def recursive(sample):
+        # move everything that is mounted here back to the root
+        session.execute(Share.__table__.update().values(mountpoint_id=0).where(Share.mountpoint_id == sample.id))
+
+        # go down the hierarchy and perform the same operations
+        for s in sample.children:
+            recursive(s)
+
+            if s.owner == current_user:
+                # if the sample down the hierarchy belongs to the current user, also mark it as deleted
+                session.execute(Sample.__table__.update().values(isdeleted=True).where(Sample.id == s.id))
+                # and delete all corresponding shares
+                session.execute(Share.__table__.delete().where(Share.sample_id == s.id))
+                # NB: if another user has children underneath this sample, they have been taken care of previously
+                #     idem if another user has mounted samples underneath this sample
+            else:
+                # otherwise it belongs to someone else and we move it back to that person's root
+                session.execute(Sample.__table__.update().values(parent_id=0).where(Sample.id == s.id))
+                # TODO: careful with duplicate sample names here
+
+    # delete all corresponding shares
+    session.execute(Share.__table__.delete().where(Share.sample_id == sample.id))
+
+    recursive(sample)
+
+
+def deleted_share_handler(session, share):
+    def recursive(sample):
+        # move samples mounted here back to the root for the user whose share we remove
+        session.execute(Share.__table__.update().values(mountpoint_id=0)
+                             .where(Share.user == share.user and Share.mountpoint == sample))
+
+        # go down the hierarchy and perform the same operations
+        for s in sample.children:
+            recursive(s)
+
+            # check if child or mounted sample belongs to the user whose share we remove and move it back to his tree
+            if s.owner == share.user:
+                session.execute(Sample.__table__.update().values(parent_id=0).where(Sample.id == s.id))
+                # TODO: careful with duplicate sample names here
+
+    recursive(share.sample)
+
+
 # https://stackoverflow.com/questions/13693872/can-sqlalchemy-events-be-used-to-update-a-denormalized-data-cache/13765857#13765857
 # https://github.com/pallets/flask-sqlalchemy/issues/182
 def after_flush(session, flush_context):
-    """ check for any deleted shares (they are also automatically deleted if the associated sample is deleted)
-        and move children or mounted samples that belong to the corresponding share user to his root
-        CAREFUL: this has to be done after the flush, because if a parent sample / mountpoint is deleted,
-        the database automatically sets the corresponding foreign keys to NULL
+    """ - check for any deleted samples or shares
+        NB: This had to be done after the flush, because if a parent sample / mountpoint was deleted,
+        the database would automatically set the corresponding foreign keys to NULL. Not sure if this
+        still applies since we are not really deleting samples anymore.
     """
+    for obj in session.dirty:
+        if isinstance(obj, Sample) and obj.isdeleted:      # detected sample deletion
+            deleted_sample_handler(session, obj)
+
     for obj in session.deleted:
-        if isinstance(obj, Share):
-            print("Detected share deletion: ", obj)
+        if isinstance(obj, Share):                         # detected share deletion
+            deleted_share_handler(session, obj)
 
-            share = obj
 
-            def reset_mountpoints(sample):
-                # check if sample is a mountpoint for the user whose share we remove and deactivate this mountpoint
-                mountedsamples = Share.query.filter_by(user=share.user, mountpoint=sample).all()
-                for s in mountedsamples:
-                    session.execute(Share.__table__.update().values(mountpoint_id=0).where(Share.id == s.id))
-                for c in sample.children+sample.mountedsamples:
-                    reset_mountpoints(c)
-
-            def find_orphans(sample):
-                for c in sample.children+sample.mountedsamples:
-                    # check if sample belongs to the user whose share we remove and move it back to his tree
-                    if c.owner == share.user:
-                        session.execute(Sample.__table__.update().values(parent_id=0).where(Sample.id == c.id))
-                    find_orphans(c)
-            
-            reset_mountpoints(share.sample)
-            find_orphans(share.sample)
-    
 event.listen(SignallingSession, 'after_flush', after_flush)
 
 
@@ -121,6 +152,8 @@ class Sample(db.Model):
     isdeleted = db.Column(db.Boolean)
     last_modified = db.Column(db.DateTime)
 
+    # NB: the cascade delete for shares and actions is no longer used because we delete samples by
+    #     setting isdeleted to True
     children = db.relationship('Sample', backref=db.backref('parent', remote_side=[id]))
     shares = db.relationship('Share', backref='sample', foreign_keys='Share.sample_id', cascade="delete")
     mountedshares = db.relationship('Share', backref='mountpoint', foreign_keys='Share.mountpoint_id')

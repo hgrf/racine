c7b5127083d2c0ff74f0ae9a6aa84c83b215539d
Holger Graef <holger.graef@gmail.com>
2022-12-23 06:30:33 +0100

    apply code style check to migrations/

--- Makefile
+++ Makefile
@@ -25,9 +25,22 @@ flake8:
 	flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 
 doc:
+	pip uninstall -y black
+	pip uninstall -y click
+	pip install black
+	pip install click
+	
+	pip uninstall -y mkdocs-material
+	pip uninstall -y requests
+	pip install mkdocs-material
+	pip install requests
+
 	# Generate documentation that points to main branch
 	# do not use custom output location, as `GitHub Pages`
 	# works only with `docs` directory
 	handsdown --external `git config --get remote.origin.url` --create-configs app --theme material -n "Mercury Sample Manager" -o docsmd
 	# generate html files to docs folder
 	python -m mkdocs build
+
+	pip install Click==7.0	# see requirements.txt
+	pip install requests==2.22.0  # see requirements.txt
--- migrations/versions/3d9e4225ecbd_remove_unique_constraint_from_sample_.py
+++ migrations/versions/3d9e4225ecbd_remove_unique_constraint_from_sample_.py
@@ -7,16 +7,17 @@ Create Date: 2016-07-04 19:25:25.851804
 """
 
 # revision identifiers, used by Alembic.
-revision = '3d9e4225ecbd'
-down_revision = '362505d00f1a'
+revision = "3d9e4225ecbd"
+down_revision = "362505d00f1a"
 
 from alembic import op
 import sqlalchemy as sa
 
 
 def upgrade():
-    op.execute('DROP INDEX ix_samples_name;')
-    op.execute('''
+    op.execute("DROP INDEX ix_samples_name;")
+    op.execute(
+        """
         CREATE TABLE samples_new (
             id INTEGER NOT NULL,
             owner_id INTEGER,
@@ -32,14 +33,17 @@ def upgrade():
             FOREIGN KEY(owner_id) REFERENCES users (id),
             FOREIGN KEY(parent_id) REFERENCES samples (id),
             FOREIGN KEY(sampletype_id) REFERENCES sampletypes (id)
-        );''')
-    op.execute('''
+        );"""
+    )
+    op.execute(
+        """
         INSERT INTO samples_new (id, owner_id, name, parent_id, sampletype_id, image, mwidth, mheight, mx, my, description,
         isarchived) SELECT id, owner_id, name, parent_id, sampletype_id, image, mwidth, mheight, mx, my, description,
         isarchived FROM samples;
-    ''')
-    op.execute('DROP TABLE samples;')
-    op.execute('ALTER TABLE samples_new RENAME TO samples;')
+    """
+    )
+    op.execute("DROP TABLE samples;")
+    op.execute("ALTER TABLE samples_new RENAME TO samples;")
 
 
 def downgrade():

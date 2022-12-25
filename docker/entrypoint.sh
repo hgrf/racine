4e55acf7ada1bbeb1a9c103f98dd4d91defd1559
Holger Graef <holger.graef@gmail.com>
2022-12-26 00:28:15 +0100

    update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 /uwsgi-nginx-flask-entrypoint.sh
 
 if [ ! -d "/app/app/static/bootstrap" ]; then
+    git clone -b 3.3.7.1 --depth 1 https://github.com/mbr/flask-bootstrap.git /flask-bootstrap
     mv /flask-bootstrap/flask_bootstrap/static /app/app/static/bootstrap
     rm -rf /flask-bootstrap
 fi
+
+python manage.py db upgrade
 
 exec "$@"

eaccf5e0e6f1a91f73165d09af919dcf5cc25d99
Holger Graef <holger.graef@gmail.com>
2023-01-16 23:55:58 +0100

    use generated JS API

---
+++ patches/rollup.config.js
@@ -0,0 +1,21 @@
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+
+export default {
+    input: 'src/index.js',
+    output: {
+        format: 'iife',
+        file: '../../app/static/msm-api.js',
+        name: 'MsmApi',
+    },
+    plugins: [
+        commonjs({esmExternals: true}),
+        json(),
+        nodePolyfills(),
+        nodeResolve({browser: true}),
+        terser(),
+    ]
+};

2a49ee5746dd65e223caa49ed1c547be056b11e5
Holger Graef <holger.graef@gmail.com>
2023-03-29 23:45:59 +0200

    refactor db handlers

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,120 +1,14 @@
-from flask_login import current_user
-from flask_sqlalchemy import SignallingSession
-from sqlalchemy import event
-
 from .action import Action  # noqa: F401
 from .activity import Activity, ActivityType, record_activity  # noqa: F401
-from .news import News, LinkUserNews
+from .news import News, LinkUserNews  # noqa: F401
 from .sample import Sample, SAMPLE_NAME_LENGTH  # noqa: F401
-from .share import Share
+from .share import Share  # noqa: F401
 from .smbresource import SMBResource  # noqa: F401
 from .upload import Upload  # noqa: F401
 from .user import token_auth, User  # noqa: F401
 
-
-def deleted_sample_handler(session, sample):
-    def recursive(sample):
-        user = token_auth.current_user() or current_user
-
-        # move everything that is mounted here back to the root
-        session.execute(
-            Share.__table__.update().values(mountpoint_id=0).where(Share.mountpoint_id == sample.id)
-        )
-
-        # go down the hierarchy and perform the same operations
-        for s in sample.children:
-            recursive(s)
-
-            if s.owner == user:
-                # if the sample down the hierarchy belongs to the current user,
-                # also mark it as deleted
-                session.execute(
-                    Sample.__table__.update().values(isdeleted=True).where(Sample.id == s.id)
-                )
-                # delete all corresponding news
-                for news in session.execute(News.__table__.select().where(News.sample_id == s.id)):
-                    news_id = news[0]
-                    session.execute(
-                        LinkUserNews.__table__.delete().where(LinkUserNews.news_id == news_id)
-                    )
-                session.execute(News.__table__.delete().where(News.sample_id == s.id))
-                # and delete all corresponding shares
-                session.execute(Share.__table__.delete().where(Share.sample_id == s.id))
-                # NB: if another user has children underneath this sample, they have been taken
-                #     care of previously; idem if another user has mounted samples underneath
-                #     this sample
-            else:
-                # otherwise it belongs to someone else and we move it back to that person's root
-                session.execute(
-                    Sample.__table__.update().values(parent_id=0).where(Sample.id == s.id)
-                )
-                # If there is some news associated with the sample or a subsample, this is not
-                # really an issue. Everybody who has access to the sample - and the person who
-                # created the news, will still see the news - but the person who created the
-                # news will not be able to access the sample (and therefore not be able to
-                # deactivate the news). Two possible solutions:
-                # - wait for the news to expire
-                # - give the user access again so he/she can deactivate the news
-                # TODO: careful with duplicate sample names here
-
-    # delete all corresponding shares
-    session.execute(Share.__table__.delete().where(Share.sample_id == sample.id))
-
-    # delete all corresponding news
-    for news in session.execute(News.__table__.select().where(News.sample_id == sample.id)):
-        news_id = news[0]
-        session.execute(LinkUserNews.__table__.delete().where(LinkUserNews.news_id == news_id))
-    session.execute(News.__table__.delete().where(News.sample_id == sample.id))
-
-    recursive(sample)
-
-
-def deleted_share_handler(session, share):
-    def recursive(sample):
-        # move samples mounted here back to the root for the user whose share we remove
-        session.execute(
-            Share.__table__.update()
-            .values(mountpoint_id=0)
-            .where(Share.user == share.user and Share.mountpoint == sample)
-        )
-
-        # go down the hierarchy and perform the same operations
-        for s in sample.children:
-            recursive(s)
-
-            # check if child or mounted sample belongs to the user whose share we remove
-            # and move it back to his tree
-            if s.owner == share.user:
-                session.execute(
-                    Sample.__table__.update().values(parent_id=0).where(Sample.id == s.id)
-                )
-                # If there is some news associated with the sample or a subsample, this is not
-                # really an issue. Everybody who has access to the sample - and the person who
-                # created the news, will still see the news - but the person who created the
-                # news will not be able to access the sample (and therefore not be able to
-                # deactivate the news). Two possible solutions:
-                # - wait for the news to expire
-                # - give the user access again so he/she can deactivate the news
-                # TODO: careful with duplicate sample names here
-
-    recursive(share.sample)
-
-
-# https://stackoverflow.com/questions/13693872/can-sqlalchemy-events-be-used-to-update-a-denormalized-data-cache/13765857#13765857
-# https://github.com/pallets/flask-sqlalchemy/issues/182
-def after_flush(session, flush_context):
-    """- check for any deleted samples or shares
-    NB: This had to be done after the flush, because if a parent sample / mountpoint was deleted,
-    the database would automatically set the corresponding foreign keys to NULL. Not sure if this
-    still applies since we are not really deleting samples anymore.
-    """
-    for obj in session.dirty:
-        if isinstance(obj, Sample) and obj.isdeleted:  # detected sample deletion
-            deleted_sample_handler(session, obj)
-
-    for obj in session.deleted:
-        if isinstance(obj, Share):  # detected share deletion
-            deleted_share_handler(session, obj)
-
+from flask_sqlalchemy import SignallingSession
+from sqlalchemy import event
+from .handlers import after_flush
 
 event.listen(SignallingSession, "after_flush", after_flush)

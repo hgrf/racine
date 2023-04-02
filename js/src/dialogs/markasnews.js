188d457cb6261b57e40174875137a9024d52eb60
Holger Graef <holger.graef@gmail.com>
2023-04-02 19:19:33 +0200

    refactor MarkAsNewsDialog

---
+++ js/src/dialogs/markasnews.js
@@ -0,0 +1,57 @@
+class MarkAsNewsDialog {
+    constructor() {
+        $('#dlg_markasnews_submit').click(function (event) {
+            event.preventDefault();
+
+            var dlg_markasnews_form = $('#dlg_markasnews_form');
+            var actionid = $("#actionid").val();
+            var flag_element = $('#togglenews-' + actionid);
+
+            // clean up error messages
+            dlg_markasnews_form.find('.form-group').removeClass('has-error');
+            dlg_markasnews_form.find('span.help-block').remove();
+
+            var formdata = {};
+            dlg_markasnews_form.serializeArray().map(function (x) { formdata[x.name] = x.value; });
+
+            R.actionsAPI.markActionAsNews(formdata, function (error, data, response) {
+                if (!response)
+                    error_dialog("Server error. Please check your connection.");
+                else if (response.body && response.body.error) {
+                    // form failed validation; because of invalid data or expired CSRF token
+                    for (var field in response.body.error) {
+                        if (field === 'csrf_token') {
+                            error_dialog('The CSRF token has expired. Please reload the page.');
+                            continue;
+                        }
+                        // get form group
+                        var formgroup = $('#' + field).closest('.form-group');
+                        // add the has-error to the form group
+                        formgroup.addClass('has-error')
+                        // add the error message to the form group
+                        for (var i in response.body.error[field]) {
+                            formgroup.append(
+                                '<span class="help-block">' +
+                                response.body.error[field][i] +
+                                '</span>'
+                            );
+                        }
+                    }
+                } else {
+                    // hide the dialog
+                    $('#dlg_markasnews').modal('hide');
+
+                    // toggle the flag
+                    flag_element.removeClass('markasnews');
+                    flag_element.addClass('unmarkasnews');
+                }
+            });
+        });
+
+        // datepicker
+        $("#expires").attr("autocomplete", "off");
+        $("#expires").datepicker({ dateFormat: "yy-mm-dd" });
+    }
+}
+
+export default MarkAsNewsDialog;
--- js/src/index.js
+++ js/src/index.js
@@ -1,6 +1,7 @@
 import * as API from "./api";
 import "lightbox2";
 import NewSampleDialog from "./dialogs/newsample";
+import MarkAsNewsDialog from "./dialogs/markasnews";
 
 class Racine {
     constructor(apiToken) {
@@ -103,57 +104,7 @@ class Racine {
         new NewSampleDialog('#newsample');
 
         // "mark as news" dialog
-        $('#dlg_markasnews_submit').click(function (event) {
-            event.preventDefault();
-
-            var dlg_markasnews_form = $('#dlg_markasnews_form');
-            var actionid = $("#actionid").val();
-            var flag_element = $('#togglenews-' + actionid);
-
-            // clean up error messages
-            dlg_markasnews_form.find('.form-group').removeClass('has-error');
-            dlg_markasnews_form.find('span.help-block').remove();
-
-            var formdata = {};
-            dlg_markasnews_form.serializeArray().map(function (x) { formdata[x.name] = x.value; });
-
-            R.actionsAPI.markActionAsNews(formdata, function (error, data, response) {
-                if (!response)
-                    error_dialog("Server error. Please check your connection.");
-                else if (response.body && response.body.error) {
-                    // form failed validation; because of invalid data or expired CSRF token
-                    for (field in response.body.error) {
-                        if (field === 'csrf_token') {
-                            error_dialog('The CSRF token has expired. Please reload the page.');
-                            continue;
-                        }
-                        // get form group
-                        var formgroup = $('#' + field).closest('.form-group');
-                        // add the has-error to the form group
-                        formgroup.addClass('has-error')
-                        // add the error message to the form group
-                        for (i in response.body.error[field]) {
-                            formgroup.append(
-                                '<span class="help-block">' +
-                                response.body.error[field][i] +
-                                '</span>'
-                            );
-                        }
-                    }
-                } else {
-                    // hide the dialog
-                    $('#dlg_markasnews').modal('hide');
-
-                    // toggle the flag
-                    flag_element.removeClass('markasnews');
-                    flag_element.addClass('unmarkasnews');
-                }
-            });
-        });
-
-        // datepicker
-        $("#expires").attr("autocomplete", "off");
-        $("#expires").datepicker({ dateFormat: "yy-mm-dd" });
+        new MarkAsNewsDialog();
 
         // set up the OK button and the enter button
         $('#userbrowserok').click(shareselected);

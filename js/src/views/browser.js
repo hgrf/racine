755d148c410f08b85c4a2c8b8f475fa84686b728
Holger Graef <holger.graef@gmail.com>
2023-04-21 21:14:34 +0200

    remove superfluous function init_browser

--- js/src/views/browser.js
+++ js/src/views/browser.js
@@ -6,7 +6,112 @@ class BrowserView {
   }
 
   onDocumentReady() {
-    init_browser();
+    if (queryDict['multi'] === 'true') {
+      set_multi_selection();
+    } else {
+      set_single_selection();
+    }
+  
+    $('#multiswitch-checkbox').click(function(event) {
+      if (this.checked) {
+        set_multi_selection();
+      } else {
+        set_single_selection();
+      }
+    });
+  
+    // tell the upload form how to communicate with the server
+    $('#uploadform').submit(function(event) {
+      event.preventDefault();
+      const formData = new FormData(this);
+  
+      // show "activity" overlay
+      $('#overlaytext').text('Uploading file...');
+      $('#overlay').css('display', 'block');
+      // send file to server
+      $.ajax({
+        url: '/browser/upload?type=img',
+        data: formData,
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        success: function(data) {
+          if (data.uploaded) {
+            parent.CKEDITOR.dialog.getCurrent().hide();	// hide the file browser
+            parent.CKEDITOR.fbcallback(data.url); // call the call back
+          } else {
+            alert('Error: '+data.error['message']);
+            $('#overlay').css('display', 'none');
+          }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          alert('Error when communicating with server.');
+        },
+      });
+    });
+  
+    function folderclickhandler(event) {
+      if (document.getElementById('multiswitch-checkbox') !== null) {
+        queryDict['multi'] = document.getElementById('multiswitch-checkbox').checked;
+      }
+      location.replace('/browser/'+$(this).data('url')+'?'+dictToURI(queryDict));
+      event.preventDefault();
+    }
+  
+    $('.folder').click(folderclickhandler);
+  
+    // TODO: note that the functionality of inpectpath and inspectresource could be easily combined and the following
+    // code could be reduced a lot by treating historyitem, resource and shortcut all the same way...
+  
+    // check for each history item if it is available
+    $('.historyitem').each(function(index, element) {
+      const historyitemdiv = $(this);
+      $.ajax({
+        url: '/browser/inspectpath',
+        type: 'post',
+        data: {'smbpath': $(this).data('url')},
+        success: function(data) {
+          if (!data.code) {
+            historyitemdiv.find('img').attr('src', '/static/images/folder.png');
+            historyitemdiv.addClass('available'); // for CSS :hover
+            // add click handler for new elements
+            historyitemdiv.click(folderclickhandler);
+          } else {
+            historyitemdiv.find('img').attr('src', '/static/images/folder_inaccessible.png');
+          }
+        },
+      });
+    });
+  
+    // check for each resource if it is available and if it has a user/sample folder
+    $('.resource').each(function(index, element) {
+      $.ajax({
+        url: '/browser/inspectresource',
+        type: 'post',
+        data: {'sampleid': queryDict['sample'],
+          'resourceid': $(this).data('id')},
+        success: function(data) {
+          const resourcediv = $('#resource' + data.resourceid);
+          const shortcutsdiv = $('#shortcuts' + data.resourceid);
+          shortcutsdiv.empty();
+          if (!data.code) {
+            if (data.userfolder != '') {
+              shortcutsdiv.append('<img class=\'shortcut\' src=\'/static/images/user.png\' data-url=\''+data.userfolder+'\'>');
+            }
+            if (data.samplefolder != '') {
+              shortcutsdiv.append('<img class=\'shortcut\' src=\'/static/images/sample.png\' data-url=\''+data.samplefolder+'\'>');
+            }
+            resourcediv.addClass('available'); // for CSS :hover
+  
+            // add click handler for new elements
+            resourcediv.click(folderclickhandler);
+            shortcutsdiv.children('img').click(folderclickhandler);
+          } else {
+            resourcediv.append(' (N/A)');
+          }
+        },
+      });
+    });
   }
 }
 
@@ -104,115 +209,6 @@ function set_multi_selection() {
           }
         },
       });
-    });
-  });
-}
-
-function init_browser() {
-  if (queryDict['multi'] === 'true') {
-    set_multi_selection();
-  } else {
-    set_single_selection();
-  }
-
-  $('#multiswitch-checkbox').click(function(event) {
-    if (this.checked) {
-      set_multi_selection();
-    } else {
-      set_single_selection();
-    }
-  });
-
-  // tell the upload form how to communicate with the server
-  $('#uploadform').submit(function(event) {
-    event.preventDefault();
-    const formData = new FormData(this);
-
-    // show "activity" overlay
-    $('#overlaytext').text('Uploading file...');
-    $('#overlay').css('display', 'block');
-    // send file to server
-    $.ajax({
-      url: '/browser/upload?type=img',
-      data: formData,
-      type: 'POST',
-      contentType: false,
-      processData: false,
-      success: function(data) {
-        if (data.uploaded) {
-          parent.CKEDITOR.dialog.getCurrent().hide();	// hide the file browser
-          parent.CKEDITOR.fbcallback(data.url); // call the call back
-        } else {
-          alert('Error: '+data.error['message']);
-          $('#overlay').css('display', 'none');
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        alert('Error when communicating with server.');
-      },
-    });
-  });
-
-  function folderclickhandler(event) {
-    if (document.getElementById('multiswitch-checkbox') !== null) {
-      queryDict['multi'] = document.getElementById('multiswitch-checkbox').checked;
-    }
-    location.replace('/browser/'+$(this).data('url')+'?'+dictToURI(queryDict));
-    event.preventDefault();
-  }
-
-  $('.folder').click(folderclickhandler);
-
-  // TODO: note that the functionality of inpectpath and inspectresource could be easily combined and the following
-  // code could be reduced a lot by treating historyitem, resource and shortcut all the same way...
-
-  // check for each history item if it is available
-  $('.historyitem').each(function(index, element) {
-    const historyitemdiv = $(this);
-    $.ajax({
-      url: '/browser/inspectpath',
-      type: 'post',
-      data: {'smbpath': $(this).data('url')},
-      success: function(data) {
-        if (!data.code) {
-          historyitemdiv.find('img').attr('src', '/static/images/folder.png');
-          historyitemdiv.addClass('available'); // for CSS :hover
-          // add click handler for new elements
-          historyitemdiv.click(folderclickhandler);
-        } else {
-          historyitemdiv.find('img').attr('src', '/static/images/folder_inaccessible.png');
-        }
-      },
-    });
-  });
-
-  // check for each resource if it is available and if it has a user/sample folder
-  $('.resource').each(function(index, element) {
-    $.ajax({
-      url: '/browser/inspectresource',
-      type: 'post',
-      data: {'sampleid': queryDict['sample'],
-        'resourceid': $(this).data('id')},
-      success: function(data) {
-        const resourcediv = $('#resource' + data.resourceid);
-        const shortcutsdiv = $('#shortcuts' + data.resourceid);
-        shortcutsdiv.empty();
-        if (!data.code) {
-          if (data.userfolder != '') {
-            shortcutsdiv.append('<img class=\'shortcut\' src=\'/static/images/user.png\' data-url=\''+data.userfolder+'\'>');
-          }
-          if (data.samplefolder != '') {
-            shortcutsdiv.append('<img class=\'shortcut\' src=\'/static/images/sample.png\' data-url=\''+data.samplefolder+'\'>');
-          }
-          resourcediv.addClass('available'); // for CSS :hover
-
-          // add click handler for new elements
-          resourcediv.click(folderclickhandler);
-          shortcutsdiv.children('img').click(folderclickhandler);
-        } else {
-          resourcediv.append(' (N/A)');
-        }
-      },
     });
   });
 }

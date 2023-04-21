89c441a47fbb533541819301dc318b8be91d3f37
Holger Graef <holger.graef@gmail.com>
2023-04-21 21:21:58 +0200

    setSingle/MultiSelection as methods

--- js/src/views/browser.js
+++ js/src/views/browser.js
@@ -6,17 +6,19 @@ class BrowserView {
   }
 
   onDocumentReady() {
+    const self = this;
+
     if (queryDict['multi'] === 'true') {
-      set_multi_selection();
+      this.setMultiSelection();
     } else {
-      set_single_selection();
+      this.setSingleSelection();
     }
   
     $('#multiswitch-checkbox').click(function(event) {
       if (this.checked) {
-        set_multi_selection();
+        self.setMultiSelection();
       } else {
-        set_single_selection();
+        self.setSingleSelection();
       }
     });
   
@@ -113,6 +115,87 @@ class BrowserView {
       });
     });
   }
+
+  setSingleSelection() {
+    // disable Save button
+    $('#savemulti').unbind('click');
+    $('#savemulti').addClass('disabled');
+  
+    // unselect selected files
+    $('.file.selected').removeClass('selected');
+  
+    // update handler for file tiles
+    $('.file').unbind('click');
+    $('.file').click(function(event) {
+      const path = $(this).data('path');
+      // show "activity" overlay
+      $('#overlaytext').text('Saving file...');
+      $('#overlay').css('display', 'block');
+      // tell the server to store the file
+      $.ajax({
+        url: '/browser/savefromsmb',
+        type: 'post',
+        data: {'path': path,
+          'type': queryDict['type']},
+        success: function(data) {
+          if (data.code) {
+            alert('Error: '+data.message);
+            $('#overlay').css('display', 'none');
+          } else {
+            // hide the file browser
+            parent.CKEDITOR.dialog.getCurrent().hide();
+            // call the call back function
+            parent.CKEDITOR.fbcallback(data.uploadurl, {'filename': data.filename, 'type': data.type});
+          }
+        },
+      });
+    });
+  }
+
+  setMultiSelection() {
+    // update handler for file tiles
+    $('.file').unbind('click');
+    $('.file').click(function(event) {
+      $(this).toggleClass('selected');
+    });
+  
+    // set up Save button
+    $('#savemulti').removeClass('disabled');
+    $('#savemulti').click(function() {
+      // count files
+      files_total = $('.file.selected').length;
+      files_left = files_total;
+      // show "activity" overlay
+      $('#overlaytext').html('Saving files... <span id="filecounter">(0/'+files_total+')</span>'+
+              '<div id="saveerrors"></div>');
+      $('#overlay').css('display', 'block');
+      // save files
+      $('.file.selected').each(function() {
+        const path = $(this).data('path');
+        // tell the server to store the file
+        $.ajax({
+          url: '/browser/savefromsmb',
+          type: 'post',
+          data: {'path': path,
+            'type': queryDict['type']},
+          success: function(data) {
+            files_left -= 1;
+            $('#filecounter').text('('+(files_total-files_left)+'/'+files_total+')');
+            if (data.code) {
+              $('#saveerrors').append('<div>Error: '+data.filename+': '+data.message+'</div>');
+            } else {
+              // call the call back function
+              parent.CKEDITOR.fbcallback(data.uploadurl, {'filename': data.filename, 'type': data.type});
+            }
+            if (!files_left && $('#saveerrors').html() == '') {
+              // hide the file browser
+              parent.CKEDITOR.dialog.getCurrent().hide();
+            }
+          },
+        });
+      });
+    });
+  }
 }
 
 // parse the query string and put it in a dictionary
@@ -129,88 +212,7 @@ function dictToURI(dict) {
   return str.join('&');
 }
 
-function set_single_selection() {
-  // disable Save button
-  $('#savemulti').unbind('click');
-  $('#savemulti').addClass('disabled');
-
-  // unselect selected files
-  $('.file.selected').removeClass('selected');
-
-  // update handler for file tiles
-  $('.file').unbind('click');
-  $('.file').click(function(event) {
-    const path = $(this).data('path');
-    // show "activity" overlay
-    $('#overlaytext').text('Saving file...');
-    $('#overlay').css('display', 'block');
-    // tell the server to store the file
-    $.ajax({
-      url: '/browser/savefromsmb',
-      type: 'post',
-      data: {'path': path,
-        'type': queryDict['type']},
-      success: function(data) {
-        if (data.code) {
-          alert('Error: '+data.message);
-          $('#overlay').css('display', 'none');
-        } else {
-          // hide the file browser
-          parent.CKEDITOR.dialog.getCurrent().hide();
-          // call the call back function
-          parent.CKEDITOR.fbcallback(data.uploadurl, {'filename': data.filename, 'type': data.type});
-        }
-      },
-    });
-  });
-}
-
 let files_total = 0;
 let files_left = 0;
-
-function set_multi_selection() {
-  // update handler for file tiles
-  $('.file').unbind('click');
-  $('.file').click(function(event) {
-    $(this).toggleClass('selected');
-  });
-
-  // set up Save button
-  $('#savemulti').removeClass('disabled');
-  $('#savemulti').click(function() {
-    // count files
-    files_total = $('.file.selected').length;
-    files_left = files_total;
-    // show "activity" overlay
-    $('#overlaytext').html('Saving files... <span id="filecounter">(0/'+files_total+')</span>'+
-            '<div id="saveerrors"></div>');
-    $('#overlay').css('display', 'block');
-    // save files
-    $('.file.selected').each(function() {
-      const path = $(this).data('path');
-      // tell the server to store the file
-      $.ajax({
-        url: '/browser/savefromsmb',
-        type: 'post',
-        data: {'path': path,
-          'type': queryDict['type']},
-        success: function(data) {
-          files_left -= 1;
-          $('#filecounter').text('('+(files_total-files_left)+'/'+files_total+')');
-          if (data.code) {
-            $('#saveerrors').append('<div>Error: '+data.filename+': '+data.message+'</div>');
-          } else {
-            // call the call back function
-            parent.CKEDITOR.fbcallback(data.uploadurl, {'filename': data.filename, 'type': data.type});
-          }
-          if (!files_left && $('#saveerrors').html() == '') {
-            // hide the file browser
-            parent.CKEDITOR.dialog.getCurrent().hide();
-          }
-        },
-      });
-    });
-  });
-}
 
 export default BrowserView;

4fa92abc39c10a49dd0c8f2df79bc607650f5fcf
Holger Graef <holger.graef@gmail.com>
2023-04-21 22:44:36 +0200

    eslint: prefer-const

--- js/src/util/substringmatcher.js
+++ js/src/util/substringmatcher.js
@@ -1,12 +1,10 @@
 const substringMatcher = function(strs) {
   return function findMatches(q, cb) {
-    let matches; let substrRegex;
-
     // an array that will be populated with substring matches
-    matches = [];
+    const matches = [];
 
     // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
+    const substrRegex = new RegExp(q, 'i');
 
     // iterate through the pool of strings and for any string that
     // contains the substring `q`, add it to the `matches` array

72a48a2cb74b858398b942cf12786dffbcb91324
Holger Graef <holger.graef@gmail.com>
2023-10-22 15:03:51 +0200

    fix docs build

--- docs/module.mk
+++ docs/module.mk
@@ -2,7 +2,7 @@ doc: api-spec
 	mv docs/module.mk ./
 
 	# generate markdown documentation
-	handsdown \
+	cd docs && handsdown \
 		--branch master \
 		--external `git config --get remote.origin.url` \
 		--cleanup \
@@ -10,34 +10,34 @@ doc: api-spec
 		--theme material \
 		--name "Racine" \
 		--output-path docsmd \
-		app \
-		--exclude app/api
+		../app \
+		--exclude ../app/api
 
-	rm requirements.mkdocs.txt
-	sed -i 's/requirements.mkdocs.txt/requirements.txt\n   - requirements: requirements-dev.txt/g' .readthedocs.yml
-	sed -i 's/mkdocs.yml/docs\/mkdocs.yml/g' .readthedocs.yml
-	cat patches/readthedocs.yml >> .readthedocs.yml
+	#rm requirements.mkdocs.txt
+	sed -i 's/requirements.mkdocs.txt/requirements.txt\n   - requirements: requirements-dev.txt/g' docs/.readthedocs.yml
+	sed -i 's/mkdocs.yml/docs\/mkdocs.yml/g' docs/.readthedocs.yml
+	cat patches/readthedocs.yml >> docs/.readthedocs.yml
 
 	# replace main page of documentation
-	cp README.md docsmd/README.md
+	cp README.md docs/docsmd/README.md
 
 	# add image for README.md
-	mkdir -p docsmd/app/static/images
-	cp app/static/images/racine.svg docsmd/app/static/images/racine.svg
+	mkdir -p docs/docsmd/app/static/images
+	cp app/static/images/racine.svg docs/docsmd/app/static/images/racine.svg
 
 	# enable code copy in documentation
-	sed -i 's/- content.code.annotate/- content.code.annotate\n    - content.code.copy/g' mkdocs.yml
+	sed -i 's/- content.code.annotate/- content.code.annotate\n    - content.code.copy/g' docs/mkdocs.yml
 
 	# add API page
-	mv swagger.json docsmd/swagger.json
-	cp patches/api.md docsmd/API.md
+	mv swagger.json docs/docsmd/swagger.json
+	cp patches/api.md docs/docsmd/API.md
 
 	# convert to HTML documentation
-	echo -n "\nplugins:\n  - render_swagger" >> mkdocs.yml
-	python -m mkdocs build
+	echo -n "\nplugins:\n  - render_swagger" >> docs/mkdocs.yml
+	cd docs && python -m mkdocs build
 
 	rm api.yaml
-	mv mkdocs.yml docs/
+	mv docs/.readthedocs.yml .
 
 	mv module.mk docs/
 

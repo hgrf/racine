49f8db3244c87fef6d82f309a43aeff6d6b4b234
Holger Graef <holger.graef@gmail.com>
2023-10-29 12:16:50 +0100

    use .spec file for pyinstaller

--- desktop/package_python.js
+++ desktop/package_python.js
@@ -1,31 +1,31 @@
 const path = require("path");
 
-const spawn = require("child_process").spawn,
-  ls = spawn(
-    "pyinstaller",
-    [
-      "-w",
-      "--onefile",
-      `--add-data app/templates${path.delimiter}templates`,
-      `--add-data app/static${path.delimiter}static`,
-      "--distpath desktop/dist-python",
-      "app/__init__.py",
-    ],
-    {
-      cwd: path.join(__dirname, ".."),
-      shell: true,
-    }
-  );
+const spawn = require("child_process").spawn;
+let env = process.env;
+env.PYTHONPATH = path.join(__dirname, "..");
+console.log(`Packaging python backend with PYTHONPATH=${env.PYTHONPATH}...`);
+let pyinstaller_proc = spawn(
+  "pyinstaller",
+  [
+    "--distpath desktop/dist-python",
+    "desktop/racine-backend.spec",
+  ],
+  {
+    cwd: path.join(__dirname, ".."),
+    env: env,
+    shell: true,
+  }
+);
 
-ls.stdout.on("data", function (data) {
+pyinstaller_proc.stdout.on("data", function (data) {
   console.log(data.toString());
 });
 
-ls.stderr.on("data", function (data) {
+pyinstaller_proc.stderr.on("data", function (data) {
   console.log("Packaging error: " + data.toString());
 });
 
-ls.on("exit", function (code) {
+pyinstaller_proc.on("exit", function (code) {
   console.log("child process exited with code " + code.toString());
   if (code !== 0)
     throw new Error("Packaging failed with code " + code.toString() + ".");
---
+++ desktop/racine-backend.spec
@@ -0,0 +1,40 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['run_app.py'],
+    pathex=['/home/holger/racine'],
+    binaries=[],
+    datas=[
+        ('../app/templates', 'app/templates'),
+        ('../app/static', 'app/static'),
+    ],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='racine-backend',
+    debug=True,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)

aef83cfdddff56b28931ea868cc627795dc4c0cd
Holger Graef <holger.graef@gmail.com>
2023-11-19 14:57:47 +0100

    set up watchman for automatic rollup

--- docker/module.mk
+++ docker/module.mk
@@ -1,28 +1,38 @@
+down:
+	docker compose -f docker/docker-compose.yml down
+
 build: down
 	docker compose -f docker/docker-compose.yml build web
 
 run:
 	docker compose -f docker/docker-compose.yml up
 
-build-dev: down
+down-dev:
+	docker compose -f docker/docker-compose-dev.yml down
+
+build-dev: down-dev
 	docker compose -f docker/docker-compose-dev.yml build web-dev
 
 test-dev:
 	docker compose -f docker/docker-compose-dev.yml exec web-dev python -m pytest
 
 watchman-rule:
+	watchman --logfile=/tmp/watchman.log watch-del-all
 	watchman watch ./app
-	cat docker/watchman.json | watchman -j
-	tail -F /dev/null
+	watchman watch ./js/src
+	cat docker/watchman-py.json | watchman -j
+	cat docker/watchman-js.json | watchman -j
+	tail -F /tmp/watchman.log
+
+watchman-rules:
+	watchman trigger-list ./app
+	watchman trigger-list ./js/src
 
 run-dev:
 	docker compose -f docker/docker-compose-dev.yml up
 
 shell-dev:
 	docker compose -f docker/docker-compose-dev.yml exec web-dev bash
-
-down:
-	docker compose -f docker/docker-compose.yml down
 
 logs:
 	docker compose -f docker/docker-compose.yml logs -f

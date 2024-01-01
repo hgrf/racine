05e668a3faef0277fa9249c563c64846ee4bdf30
Holger Graef <holger.graef@gmail.com>
2024-01-01 14:52:19 +0000

    minify JS

--- js/rollup.config.js
+++ js/rollup.config.js
@@ -1,8 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import nodeResolve from '@rollup/plugin-node-resolve';
-// import json from '@rollup/plugin-json';
-// import terser from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.js',
@@ -15,7 +14,6 @@ export default {
     commonjs({esmExternals: true}),
     nodePolyfills(),
     nodeResolve({browser: true}),
-    // json(),
-    // terser(),
+    terser(),
   ],
 };
